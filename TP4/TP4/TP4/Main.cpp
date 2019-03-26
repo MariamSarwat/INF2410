7812dfaa88426378885c18a148d30de0d8045738
Mariam Sarwat <sarwatmariam@gmail.com>
2019-03-25 22:04:29 -0400

    repush

---
+++ TP4/TP4/TP4/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <exception>
+
+#include "Test_TP4.h"
+
+int main() {
+	try {
+		Test_TP4 TestCorrection;
+
+		TestCorrection.executeProxyTest();
+		TestCorrection.executeCompositeTest();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception lancee" << std::endl;
+		std::cerr << e.what() << std::endl;
+	}
+
+	//return 0;
+	system("pause");
+}
