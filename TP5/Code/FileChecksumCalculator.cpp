0f979821fe1962c04292c239b87cf8be7d159943
Mariam Sarwat <sarwatmariam@gmail.com>
2019-03-27 11:00:20 -0400

    part one code finished

--- TP5/Code/FileChecksumCalculator.cpp
+++ TP5/Code/FileChecksumCalculator.cpp
@@ -5,21 +5,26 @@
 #include "MemAudioFile.h"
 #include "ChunkIterator.h"
 
-FileChecksumCalculator * FileChecksumCalculator::clone(void)
-{
+FileChecksumCalculator * FileChecksumCalculator::clone(void) {
 	return new FileChecksumCalculator(*this);
 }
 
-void FileChecksumCalculator::visit(const AudioFile & f)
-{
+void FileChecksumCalculator::visit(const AudioFile & f) {
 	// Initialiser le checksum avec une valeur non-nulle
 	m_checkSum = 0xabcdef00;
 
 	// A COMPLETER:
 	// Iterer sur chacun des Chunk
-	//    - Calculer le hash du Chunk avec computeHash
-	//    - Combiner le checksum, le hash du Chunk et l'index du Chunk avec combineHash
-	//    - Conserver le resultat comme nouveau checksum
+	size_t index = 0;
+	for (auto it = f.begin(); it != f.end(); it++) {
+		// - Calculer le hash du Chunk avec computeHash
+		uint64_t hash = computeChunkHash(it->size(), it->get());
+
+		//- Combiner le checksum, le hash du Chunk et l'index du Chunk avec combineHash
+		//- Conserver le resultat comme nouveau checksum
+		m_checkSum = combineHash(m_checkSum, hash, index);
+		index++;
+	}
 }
 
 void FileChecksumCalculator::visit(const MemAudioFile & f)
@@ -30,13 +35,22 @@ void FileChecksumCalculator::visit(const MemAudioFile & f)
 	size_t chunkSize = f.getChunkSize();
 	size_t nChunks = f.getNumberChunks();
 	const char* buf = f.get()+f.getHeaderSize();
-
+	
 	// A COMPLETER:
-	// Iterer sur chacun des Chunk
-	//    - Calculer le hash du Chunk (portion de buf) avec computeHash
-	//    - Combiner le checksum, le hash du Chunk et l'index du Chunk avec combineHash
-	//    - Conserver le resultat comme nouveau checksum
-	//    - Avancer dans buf
+		// Iterer sur chacun des Chunk
+	size_t index = 0;
+	for (auto it = f.begin(); it != f.end(); it++) {
+		//- Calculer le hash du Chunk (portion de buf) avec computeHash
+		uint64_t hash = computeChunkHash(chunkSize, buf);
+		
+		//- Combiner le checksum, le hash du Chunk et l'index du Chunk avec combineHash
+		//- Conserver le resultat comme nouveau checksum
+		m_checkSum = combineHash(m_checkSum, hash, index);
+		index++;
+		
+		//- Avancer dans buf
+		buf = it->get();
+	}
 }
 
 uint64_t FileChecksumCalculator::computeChunkHash(size_t chunkSize, const char* chunkData)
--- TP5/Code/FileStringFindReplace.cpp
+++ TP5/Code/FileStringFindReplace.cpp
@@ -19,14 +19,22 @@ FileStringFindReplace * FileStringFindReplace::clone(void)
 	return new FileStringFindReplace(*this);
 }
 
-void FileStringFindReplace::visit(const AudioFile & f)
-{
+void FileStringFindReplace::visit(const AudioFile & f) {
 	// A COMPLETER:
 	// Iterer sur chaque Chunk du fichier d'entree
-	//    - Construire un nouvel iterateur pointant a la fin du fichier de sortie
-	//    - Initialiser la memoire de l'iterateur avec le contenu du Chunk d'entree
-	//    - Remplacer les caracteres dans le Chunk de sortie avec findReplace
-	//    - Ajouter le nouveau Chunk au fichier de sortie
+	for (auto it = f.begin(); it != f.end(); it++) {
+		//-Construire un nouvel iterateur pointant a la fin du fichier de sortie
+		Chunk_iterator iterator(*m_outFile, m_outFile->getChunkSize());
+		
+		//- Initialiser la memoire de l'iterateur avec le contenu du Chunk d'entree
+		memcpy(iterator->get(), it->get(), iterator->size());
+
+		//- Remplacer les caracteres dans le Chunk de sortie avec findReplace
+		findReplace(iterator->get(), iterator->get() + iterator->size());
+
+		//- Ajouter le nouveau Chunk au fichier de sortie
+		m_outFile->addChunk(iterator);
+	}
 }
 
 void FileStringFindReplace::visit(const MemAudioFile & f)
@@ -34,13 +42,25 @@ void FileStringFindReplace::visit(const MemAudioFile & f)
 	size_t chunkSize = f.getChunkSize();
 	size_t nChunks = f.getNumberChunks();
 	const char* buf = f.get() + f.getHeaderSize();
+
 	// A COMPLETER:
 	// Iterer sur chaque Chunk du fichier d'entree dans buf
-	//    - Construire un nouvel iterateur pointant a la fin du fichier de sortie
-	//    - Initialiser la memoire de l'iterateur avec le contenu du Chunk d'entree
-	//    - Remplacer les caracteres dans le Chunk de sortie avec findReplace
-	//    - Ajouter le nouveau Chunk au fichier de sortie
-	//    - Avancer dans buf
+	for (auto it = f.begin(); it != f.end(); it++) {
+		//- Construire un nouvel iterateur pointant a la fin du fichier de sortie
+		Chunk_iterator iterator(*m_outFile, chunkSize);
+
+		//- Initialiser la memoire de l'iterateur avec le contenu du Chunk d'entree
+		memcpy(iterator->get(), buf, iterator->size());
+
+		//- Remplacer les caracteres dans le Chunk de sortie avec findReplace
+		findReplace(iterator->get(), iterator->get() + iterator->size());
+
+		//- Ajouter le nouveau Chunk au fichier de sortie
+		m_outFile->addChunk(iterator);
+
+		//- Avancer dans buf
+		buf = it->get();
+	}
 }
 
 void FileStringFindReplace::findReplace(char * p_beg, char * p_end)
